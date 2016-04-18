fad65b2aa7b623585f1bfb3ba0fbddebf57a8f5e
jonasgiek <jonasgiek@h188-120-171-220.cust.se.alltele.net>
2016-04-18 15:49:51 +0200

    Lägger till filer från Eclipse och Maven.

---
+++ JoSiProject/src/test/java/ReadVCF.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author jonasgiek
+ *attempt at reading a VCF
+ */
+import htsjdk.samtools.*;
+
+public class ReadVCF {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
