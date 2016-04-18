0337a91bc56f886dc4ffd57c069c159c0ba3c1bb
JonasGiek <jonas.lf.giek@gmail.com>
2016-04-18 08:34:20 +0200

    Added files via upload

---
+++ ReadVCF.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author jonasgiek
+ *
+ */
+public class ReadVCF {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
