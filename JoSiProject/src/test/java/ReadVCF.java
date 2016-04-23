d0fcfaaa8b5410390bd22c300f13920387d67ef4
jonasgiek <jonasgiek@h188-120-171-220.cust.se.alltele.net>
2016-04-23 19:37:55 +0200

    tested around with import and git

--- JoSiProject/src/test/java/ReadVCF.java
+++ JoSiProject/src/test/java/ReadVCF.java
@@ -1,21 +1,23 @@
-/**
- * 
- */
-
-/**
- * @author jonasgiek
- *attempt at reading a VCF
- */
-import htsjdk.samtools.*;
-
-public class ReadVCF {
-
 	/**
-	 * @param args
+	 * 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	/**
+	 * @author jonasgiek
+	 *attempt at reading a VCF
+	 */
+	import htsjdk.*;
+	import htsjdk.variant.vcf.VCFFileReader;
+	
+	import java.io.*;
+	public class ReadVCF {
+	
+		/**
+		 * @param args
+		 */
+		public static void main(String[] args) {
 
+			file vcf = 
+		
+		}
 	}
-
-}
