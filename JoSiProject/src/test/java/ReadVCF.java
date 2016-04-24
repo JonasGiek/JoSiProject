b054686f01a111b8641c1706c8d6fa1b544682b4
jonasgiek <jonasgiek@h188-120-171-220.cust.se.alltele.net>
2016-04-24 20:35:13 +0200

    Alot of error still, but outline.

--- JoSiProject/src/test/java/ReadVCF.java
+++ JoSiProject/src/test/java/ReadVCF.java
@@ -1,23 +1,25 @@
-	/**
-	 * 
-	 */
-	
-	/**
-	 * @author jonasgiek
-	 *attempt at reading a VCF
-	 */
-	import htsjdk.*;
-	import htsjdk.variant.vcf.VCFFileReader;
-	
-	import java.io.*;
-	public class ReadVCF {
-	
 		/**
-		 * @param args
+		 * 
 		 */
-		public static void main(String[] args) {
-
-			file vcf = 
 		
+		/**
+		 * @author jonasgiek
+		 *attempt at reading a VCF
+		 */
+	
+import htsjdk.samtools.util.IntervalList;
+import htsjdk.variant.vcf.VCFFileReader;
+import java.io.*;
+		public class ReadVCF {
+			
+			/**
+			 * @param args
+			 */
+			public static void main(String[] args) {
+				
+				File vcfFile = new File("trio.2010_06.ychr.sites.vcf.gz");
+				VCFFileReader reader=new VCFFileReader(vcfFile);
+				IntervalList list = reader.fromVcf(reader);
+			
+			}
 		}
-	}
